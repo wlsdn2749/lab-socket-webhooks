4a201e4fdd3c0f89908acdb79cc76d194cdd1438
wlsdn2749 <wlsdn2749@naver.com>
2023-11-06 19:41:20 +0900

    update messages 1

--- tcp-server.py
+++ tcp-server.py
@@ -14,7 +14,7 @@ while True:
     conn, addr = s.accept()
     print('Client address:', addr)
     data = conn.recv(BUFFER_SIZE)
-    cur_time = ' ' + " new server!!" + time.ctime(time.time()) + "\r\n"
+    cur_time = ' ' + " new server!!!" + time.ctime(time.time()) + "\r\n"
     print(data.decode('utf-8'))
     data = data + cur_time.encode('ascii')
     conn.send(data)
